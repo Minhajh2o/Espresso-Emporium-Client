77b61f52ee4c5b568d71e32adb55eb0859515a5d
Minhajul Islam <mislam2330718@bscse.uiu.ac.bd>
2025-12-04 05:23:02 +0600

    a small corrections added at banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -4,11 +4,11 @@ import bannerBg from "../assets/more/3.png";
 const Banner = () => {
   return (
     <div
-      className="bg-cover bg-center min-h-[800px] flex items-center"
+      className="bg-cover bg-center min-h-svh flex items-center"
       style={{ backgroundImage: `url(${bannerBg})` }}
     >
       <div className="container max-w-6xl mx-auto px-4">
-        <div className="max-w-3xl ml-auto text-white pl-[89px]">
+        <div className="max-w-2xl ml-0 md:ml-auto text-white">
           <h1 className="text-6xl mb-4">
             Would you like a Cup of Delicious Coffee?
           </h1>
