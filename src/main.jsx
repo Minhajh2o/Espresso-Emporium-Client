b808797b69f19cf142377527ff508b10f29aec43
Minhajul Islam <mislam2330718@bscse.uiu.ac.bd>
2025-12-06 01:37:24 +0600

    route importe fixed at main but showing error

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import router from "./Routes/router";
 import { RouterProvider } from "react-router/dom";
 import AuthProvider from "./context/AuthProvider";
+import router from "./routes/router";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
